f0f1e6213db284d796d49bc9137508e9d2033e54
GalGog <galynna00@gmail.com>
2018-10-26 15:40:30 +0100

    add Simple Component abd class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './Table';
+
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <h1>Hello, React!</h1>
+                <Table />
             </div>
         );
     }
